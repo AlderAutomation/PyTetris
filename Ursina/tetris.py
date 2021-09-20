a9326e5da37594bbd52e1f2666a77c96b169cfef
CodingCurtis42 <codingcurtis42@gmail.com>
2021-09-20 06:12:25 -0700

    Clean Dirs. Resolve play-area not showing.

--- Ursina/tetris.py
+++
@@ -1,8 +0,0 @@
-from ursina import *
-import tetrimos
-
-app = Ursina(title="PyTetris", vsync=False, fullscreen=False)
-
-tetrimos.tetris_board()
-
-app.run()
--- mainapp.py
+++ mainapp.py
@@ -9,22 +9,35 @@ win_x = 800
 win_y = 600
 
 win = pygame.display.set_mode((win_x, win_y))
+bg = pygame.image.load("assets/forrest.png")
+bg = pygame.transform.scale(bg, (win_x, win_y))
 pygame.display.set_caption("PyTetris")
 run = True
 clock = pygame.time.Clock()
+
+
+
+class piece():
+    def __init__(self) -> None:
+        self.x = 125
+        self.y = 50
+        self.vel = 5
+
+
+
     
 
 def redraw_win():
-    win.fill((0,0,0))
+    win.blit(bg, (0,0))
     pygame.display.update()
 
 
-play_x = 300
-play_y = 100
-play_width = 200
-play_hieght = 400
-play_area = pygame.draw.rect(win, (255,0,0), (play_x, play_y, play_width, play_hieght))
-
+def draw_play_area():
+    play_x = 275
+    play_y = 50
+    play_width = 250
+    play_hieght = 500
+    pygame.draw.rect(win, (0,0,0), (play_x, play_y, play_width, play_hieght))
 
 
 while run:
@@ -34,7 +47,9 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.draw.rect(win, (255,0,0), (play_x, play_y, play_width, play_hieght))
     redraw_win()
+    draw_play_area()
+    pygame.display.update()
+
 
 pygame.quit()
