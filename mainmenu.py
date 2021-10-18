ebfa126e64122d700cf59ccf56867b992ca862a0
CodingCurtis42 <codingcurtis42@gmail.com>
2021-10-18 13:27:56 -0700

    move cursor vars into class vars

--- mainmenu.py
+++ mainmenu.py
@@ -20,6 +20,7 @@ class main_menu:
 
 
     def draw_game_cursor(self, x: int, y: int, flip=True):
+        self.game_cursor = "A"
         self.g_cursor_x, self.g_cursor_y = x, y
         self.cursor = pygame.image.load("assets/pics/Cursor.png")
         self.menu_win.blit(self.cursor, (self.g_cursor_x, self.g_cursor_y))
@@ -28,6 +29,7 @@ class main_menu:
 
 
     def draw_music_cursor(self, x: int, y: int):
+        self.music_cursor = "1"
         self.m_cursor_x, self.m_cursor_y = x, y
         self.cursor = pygame.image.load("assets/pics/Cursor.png")
         self.menu_win.blit(self.cursor, (self.m_cursor_x, self.m_cursor_y))
@@ -40,8 +42,6 @@ def main():
 
     main = main_menu(800, 600)
     main.draw_main()
-    game_cursor = "A"
-    music_cursor = "1"
     clock = pygame.time.Clock()
 
     run = True
@@ -64,50 +64,50 @@ def main():
                 pygame.quit()
             # game cursor
             if event.key == pygame.K_d:
-                if game_cursor == "A":
+                if main.game_cursor == "A":
                     main.redraw_win()
                     main.draw_game_cursor(495, 148)
                     main.draw_music_cursor(main.m_cursor_x, main.m_cursor_y)
-                    game_cursor = "B"
+                    main.game_cursor = "B"
             if event.key == pygame.K_a:
-                if game_cursor == "B":
+                if main.game_cursor == "B":
                     main.redraw_win()
                     main.draw_game_cursor(195, 148)
                     main.draw_music_cursor(main.m_cursor_x, main.m_cursor_y)
-                    game_cursor = "A"
+                    main.game_cursor = "A"
             # music cursor
             if event.key == pygame.K_s:
-                if music_cursor == "1":
+                if main.music_cursor == "1":
                     main.redraw_win()
                     main.draw_music_cursor(320, 405)
                     main.draw_game_cursor(main.g_cursor_x, main.g_cursor_y)
-                    music_cursor = "2"
-                elif music_cursor == "2":
+                    main.music_cursor = "2"
+                elif main.music_cursor == "2":
                     main.redraw_win()
                     main.draw_music_cursor(320, 447)
                     main.draw_game_cursor(main.g_cursor_x, main.g_cursor_y)
-                    music_cursor = "3"
-                elif music_cursor == "3":
+                    main.music_cursor = "3"
+                elif main.music_cursor == "3":
                     main.redraw_win()
                     main.draw_music_cursor(320, 490)
                     main.draw_game_cursor(main.g_cursor_x, main.g_cursor_y)
-                    music_cursor = "off"
+                    main.music_cursor = "off"
             if event.key == pygame.K_w:
-                if music_cursor == "off":
+                if main.music_cursor == "off":
                     main.redraw_win()
                     main.draw_music_cursor(320, 447)
                     main.draw_game_cursor(main.g_cursor_x, main.g_cursor_y)
-                    music_cursor = "3"
-                elif music_cursor == "3":
+                    main.music_cursor = "3"
+                elif main.music_cursor == "3":
                     main.redraw_win()
                     main.draw_music_cursor(320, 405)
                     main.draw_game_cursor(main.g_cursor_x, main.g_cursor_y)
-                    music_cursor = "2"
-                elif music_cursor == "2":
+                    main.music_cursor = "2"
+                elif main.music_cursor == "2":
                     main.redraw_win()
                     main.draw_music_cursor(320, 360)
                     main.draw_game_cursor(main.g_cursor_x, main.g_cursor_y)
-                    music_cursor = "1"
+                    main.music_cursor = "1"
 
     
     pygame.quit()
