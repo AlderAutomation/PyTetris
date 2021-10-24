15a916011aaab7ac9edd6840892ac908b4b85556
CodingCurtis42 <codingcurtis42@gmail.com>
2021-10-23 21:48:05 -0700

    create & draw grid. fix game resolution. fix text placements

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ class game():
         self.game_bg = pygame.image.load("assets/pics/playfield.png")
         self.game_bg = pygame.transform.scale(self.game_bg, (self.game_x, self.game_y))
         self.game_win = pygame.display.set_mode((self.game_x, self.game_y))
-        self.game_text = pygame.font.Font("assets/Tetris.ttf", 25)
+        self.game_text = pygame.font.Font("assets/Tetris.ttf", 24)
         self.white = (255,255,255)
         self.black = (0,0,0)
         self.score = 0
@@ -25,13 +25,17 @@ class game():
         self.s_count = 0
         self.l_count = 0
         self.i_count = 0
-    
+        # self.play_width, self.play_height = 250, 500
+        self.grid = self.create_grid()
+        self.block_size = 24
+
 
     def draw_win(self) -> None:
         self.game_win.blit(self.game_bg, (0,0))
         self.draw_scores()
         self.draw_level()
         self.draw_lines()
+        self.draw_grid(self.grid)
         self.update_piece_count()
         pygame.display.update()
 
@@ -49,8 +53,8 @@ class game():
     
 
     def draw_scores(self) -> None:
-        score = stat_builder(self.score, 600, 150, self)
-        hiscore = stat_builder(self.hi_score_1, 600, 90, self)
+        score = stat_builder(self.score, 578, 170, self)
+        hiscore = stat_builder(self.hi_score_1, 578, 98, self)
 
 
     def load_hi_scores(self) -> None:
@@ -64,7 +68,7 @@ class game():
 
 
     def draw_level(self) -> None:
-        stat_builder(self.lvl, 650, 430, self, 2)
+        stat_builder(self.lvl, 625, 480, self, 2)
 
 
     def update_piece_count(self) -> None:
@@ -77,13 +81,12 @@ class game():
         I_count = stat_builder(self.i_count, 150, 480, self, 3)
 
 
-
     def display_pieces_for_count(self) -> None:
         pass
 
 
     def draw_lines(self) -> None:
-        stat_builder(self.lines, 475, 42, self, 3)
+        stat_builder(self.lines, 460, 48, self, 3)
 
 
     def on_lose(self) -> None:
@@ -96,6 +99,25 @@ class game():
         pass
 
 
+    def create_grid(self, locked_pos = {}) -> object:
+        grid = [[(self.white)for _ in range(10)] for _ in range(20)]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if (j, i) in locked_pos:
+                    c = locked_pos[(j,i)]
+                    grid[i][j] = c
+            
+            return grid
+
+
+    def draw_grid(self, grid: object) -> None:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                pygame.draw.rect(self.game_win, grid[i][j], (287+j*self.block_size, 121+i*self.block_size, self.block_size, self.block_size), 1)
+
+
+        
     def screen_input(self) -> None:
         pass
 
@@ -134,6 +156,6 @@ class stat_builder:
 
 
 if __name__== "__main__":
-    Tetris = game(800, 600, 0).main()
+    Tetris = game(768, 672, 0).main()
     Tetris
     
